d94985a0fe674eb43dc3895bdb38a0361fd7d9f6
Alexander Dutton <alexander.dutton@it.ox.ac.uk>
2013-12-09 14:42:21 +0000

    Added user groups, split from service groups

--- src/static/app/model.js
+++ src/static/app/model.js
@@ -15,13 +15,15 @@ define(['backbone', 'underscore'], function(Backbone, _) {
 
 	var Categories = Backbone.Collection.extend({
 		model: Category,
-		url: "https://data.ox.ac.uk/search/?format=json&q=scheme.uri:%22https://id.it.ox.ac.uk/service-category%22",
+		url: "https://data.ox.ac.uk/search/?format=json&q=scheme.uri:%22https://id.it.ox.ac.uk/service-category%22&page_size=20",
 		parse: function(response) {
 			return _.map(response.hits.hits, function(e) {
 				return {
 					label: e._source.label,
 					fontAwesome: e._source.fontAwesome,
 					slug: e._source.notation.serviceCategory,
+					teaser: e._source.teaser,
+					broader: e._source.broader[0].uri,
 					featured: e._source.featured ? _.map(e._source.featured, function(e) {
 						return e.uri;
 					}) : [],
